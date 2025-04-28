1348494169977f3648860e29e5337e4633a3b6e2
vishnu <139614960+vishnuu5@users.noreply.github.com>
2025-04-28 12:47:44 +0530

    firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,19 +1,17 @@
+// firebase.ts
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCTn0l9wKYkgotim5mKXoJynPOSsKCxwHU",
-  authDomain: "getwell-saas.firebaseapp.com",
-  projectId: "getwell-saas",
-  storageBucket: "getwell-saas.firebasestorage.app",
-  messagingSenderId: "111685203803",
-  appId: "1:111685203803:web:939f2a46de3bd5de09cc7e",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 export default app;
